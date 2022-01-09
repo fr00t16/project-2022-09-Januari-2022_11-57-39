1dd170874635ff64161bd8353330d8a8fd151a5e
Faizal R <faizal.rifaldy@gmail.com>
2022-01-09 11:57:41 +0700

    commit 1666978881

--- project-2022-09-Januari-2022_11-57-39.py
+++ project-2022-09-Januari-2022_11-57-39.py
@@ -1,8 +1,8 @@
-# write by number int dari 0 - 532
+# write by number int dari 0 - 251
 # for test apps form this project 
 from random import seed
 from random import randint
 seed(1)
-for _ in range(532):
-	value = randint(0,532)
+for _ in range(251):
+	value = randint(0,251)
 	print(value)
